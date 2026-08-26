ce996c5986f64bf3fe93c2add5cbf48733d1a0fb
Eric Blyth <ericblyth56@gmail.com>
2026-08-26 15:09:55 +1000

    feat: add main menu with placeholder menu controller code

---
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void onRunSystemButtonClick()
+    {
+        //SceneManager.LoadScene("RunSystem");
+    }
+
+    public void onTutorialButtonClick()
+    {
+        //SceneManager.LoadScene("Tutorial");
+    }
+}
